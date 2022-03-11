f544e3f63bf1f2c339e81d6b8602720845ce1fea
Ritunjay kumar <56359843+ritunjaykumar@users.noreply.github.com>
2022-03-11 16:19:47 +0530

    Update Credentials.java

--- app/src/main/java/com/softgyan/moviesapp/utils/Credentials.java
+++ app/src/main/java/com/softgyan/moviesapp/utils/Credentials.java
@@ -3,7 +3,7 @@ package com.softgyan.moviesapp.utils;
 public class Credentials {
 
     public static final String BASE_URL = "https://api.themoviedb.org";
-    public static final String API_KEY = "cff01f15ed5d8af009747c76abd45aca";
+    public static final String API_KEY = "sfsdfsdfsfsfsdfs";
 
 
 }
